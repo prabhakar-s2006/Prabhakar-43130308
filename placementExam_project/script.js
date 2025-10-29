0ed54e7f0b6a5aad791594bde0df4aed5aa918d2
prabhakar-s2006 <prabhaabu08@gmail.com>
2025-10-29 16:56:39 +0530

    Add files via upload

---
+++ placementExam_project/script.js
@@ -0,0 +1,35 @@
+
+
+document.getElementById("addBtn").onclick = function() {
+
+  
+  let name = document.getElementById("expenseName").value;
+  let amount = document.getElementById("expenseAmount").value;
+  let category = document.getElementById("expenseCategory").value;
+  let date = document.getElementById("expenseDate").value;
+
+ 
+  
+  
+  let newExpense = document.createElement("div");
+  newExpense.className = "expense-box";
+  newExpense.innerHTML = name + " - ₹" + amount + "Category: "+ category + "Date:  " + date;
+
+  
+  let delButton = document.createElement("button");
+  delButton.innerText = "X";
+  delButton.className = "delete-btn";
+
+  
+  delButton.onclick = function() {
+    newExpense.remove();
+  }
+
+  
+  newExpense.appendChild(delButton);
+ document.getElementById("expenseContainer").appendChild(newExpense);
+  document.getElementById("expenseName").value = "";
+  document.getElementById("expenseAmount").value = "";
+  document.getElementById("expenseCategory").value = "";
+  document.getElementById("expenseDate").value = "";
+}
