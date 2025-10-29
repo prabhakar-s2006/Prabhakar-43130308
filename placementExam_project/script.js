d21fa7b2ff8e5d57b0cdb9c1c0b638af7b4b32c6
prabhakar-s2006 <prabhaabu08@gmail.com>
2025-10-29 15:35:42 +0530

    Add files via upload

---
+++ placementExam_project/script.js
@@ -0,0 +1,38 @@
+
+
+document.getElementById("addBtn").onclick = function() {
+
+  
+  let name = document.getElementById("expenseName").value;
+  let amount = document.getElementById("expenseAmount").value;
+  let category = document.getElementById("expenseCategory").value;
+  let date = document.getElementById("expenseDate").value;
+
+ 
+  if (name == "" || amount == "" || category == "" || date == "") {
+    return;
+  }
+
+  
+  let newExpense = document.createElement("div");
+  newExpense.className = "expense-box";
+  newExpense.innerHTML = name + " - ₹" + amount + "<br>" + category + " | " + date;
+
+  
+  let delButton = document.createElement("button");
+  delButton.innerText = "X";
+  delButton.className = "delete-btn";
+
+  
+  delButton.onclick = function() {
+    newExpense.remove();
+  }
+
+  
+  newExpense.appendChild(delButton);
+ document.getElementById("expenseContainer").appendChild(newExpense);
+  document.getElementById("expenseName").value = "";
+  document.getElementById("expenseAmount").value = "";
+  document.getElementById("expenseCategory").value = "";
+  document.getElementById("expenseDate").value = "";
+}
